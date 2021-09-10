4b2a506d6a884128ea5c202d8992403007823f2c
visual <visual@LAB24-01>
2021-09-10 17:56:37 +0300

    Добавьте файлы проекта.

---
+++ xamarin-pract-3/xamarin-pract-3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_pract_3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Accept_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Exit_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
